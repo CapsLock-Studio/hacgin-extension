44b0658c19de01ae3e812768ee02bcfcf5ea9ba6
michael34435 <michael34435@gmail.com>
2015-03-14 02:04:05 +0800

    initial commit

---
+++ js/background.js
@@ -0,0 +1,23 @@
+$(function() {
+    var data = []
+    var content = "";
+    $.each($("p"), function(k, v) {
+        var text = $(v).text();
+        var m;
+        if (m = text.match(/[0-9a-f]{40}/gi)) {
+            data.push(m[0]);
+        }
+    });
+
+    if (data.length == 0) {
+        return false;
+    }
+
+    for (var key in data) {
+        var link = '<a target="_blank" href="magnet:?xt=urn:btih:' + data[key] + '">下載 ' + (key + 1) + '</a>';
+        content += link;
+    }
+
+    $("body").append($("<div id='dialog'>" + link + "</div>"));
+    $("#dialog").dialog();
+});
