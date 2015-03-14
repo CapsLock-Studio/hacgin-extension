680cdd23f76316a248e504c445a1b7e3a930b554
michael34435 <michael34435@gmail.com>
2015-03-14 09:36:13 +0800

    bug fixed

--- js/background.js
+++ js/background.js
@@ -4,18 +4,19 @@ $.each($("p, pre"), function(k, v) {
     var text = $(v).text();
     var m;
     if (m = text.match(/[0-9a-fA-F]{40}/gi)) {
-        if (m[0] != undefined) {
-            data.push(m[0]);
+        for (var sub_key in m) {
+            data.push(m[sub_key]);
         }
     }
 });
 
 if (data.length != 0) {
     for (var key in data) {
-        var link = '<a target="_blank" href="magnet:?xt=urn:btih:' + data[key] + '">下載 ' + (key + 1) + '</a>';
+        var number = parseInt(key) + 1
+        var link = '<a target="_blank" href="magnet:?xt=urn:btih:' + data[key] + '">下載 ' + number + '</a><br>';
         content += link;
     }
 
-    $("body").append($("<div id='dialog'>" + link + "</div>"));
+    $("body").append($("<div id='dialog' style='text-align: center;'>" + content + "</div>"));
     $("#dialog").dialog();
 }
